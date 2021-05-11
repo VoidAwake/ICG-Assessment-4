5012d130cad10bd22afe73e473c971dac469758f
aabb1998 <53510613+aabb1998@users.noreply.github.com>
2021-05-11 23:37:51 +1000

    Added lighting gui. (not completed)

--- js/Perspective/setup.js
+++ js/Perspective/setup.js
@@ -1,6 +1,8 @@
-import {build} from "./buildFloor.js";
-import {PointerLockControls} from "../Dependencies/PointerLockControls.js";
-import * as THREE from "../Dependencies/three.module.js";
+import { build } from './buildFloor.js';
+import { PointerLockControls } from '../Dependencies/PointerLockControls.js';
+import * as THREE from '../Dependencies/three.module.js';
+import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/controls/OrbitControls.js';
+import { GUI } from 'https://threejsfundamentals.org/threejs/../3rdparty/dat.gui.module.js';
 
 var camera, scene, renderer, controls;
 var controlsEnabled = true;
@@ -24,188 +26,265 @@ var clock;
 //var playerMesh = new THREE.Mesh(playerGeo, playerMaterial);
 
 function init() {
-	clock = new THREE.Clock();
-	clock.start();
-	var ratio = window.innerWidth/window.innerHeight;
-	camera = new THREE.PerspectiveCamera(45,ratio,0.00001,1000);
+  clock = new THREE.Clock();
+  clock.start();
+  var ratio = window.innerWidth / window.innerHeight;
+  camera = new THREE.PerspectiveCamera(45, ratio, 0.00001, 1000);
 
-	var Pos = new THREE.Vector3(0,2,0);
-	camera.position.set(Pos.x,Pos.y,Pos.z);
-	var Dir = new THREE.Vector3(0, 0, 1);
-	camera.lookAt(Dir.x,Dir.y,Dir.z);
-	scene = new THREE.Scene();
+  var Pos = new THREE.Vector3(0, 2, 0);
+  camera.position.set(Pos.x, Pos.y, Pos.z);
+  var Dir = new THREE.Vector3(0, 0, 1);
+  camera.lookAt(Dir.x, Dir.y, Dir.z);
+  scene = new THREE.Scene();
 
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
-	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	document.body.appendChild( renderer.domElement );
-	controls = new PointerLockControls(camera, renderer.domElement);
-//	scene.add(playerMesh);
-	document.addEventListener( 'click', function () {
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  controls = new PointerLockControls(camera, renderer.domElement);
+  //	scene.add(playerMesh);
+  document.addEventListener(
+    'click',
+    function () {
+      controls.lock();
+    },
+    false
+  );
 
-		controls.lock();
-	}
-	, false );
+  controls.enabled = true;
+  scene.add(controls.getObject());
 
-controls.enabled = true;
-	scene.add( controls.getObject() );
+  var onKeyDown = function (event) {
+    switch (event.keyCode) {
+      case 38: // up
+      case 87: // w
+        moveForward = true;
+        break;
 
-	var onKeyDown = function ( event ) {
+      case 37: // left
+      case 65: // a
+        moveLeft = true;
+        break;
 
-		switch ( event.keyCode ) {
+      case 40: // down
+      case 83: // s
+        moveBackward = true;
+        break;
 
-			case 38: // up
-			case 87: // w
-				moveForward = true;
-				break;
+      case 39: // right
+      case 68: // d
+        moveRight = true;
+        break;
+      case 32:
+        //	called = true;
+        //		rising = true;
+        break;
+    }
+  };
+  var time = 0;
+  var delta = 0;
 
-			case 37: // left
-			case 65: // a
-				moveLeft = true;
-  break;
+  var onKeyUp = function (event) {
+    switch (event.keyCode) {
+      case 38: // up
+      case 87: // w
+        moveForward = false;
+        break;
 
-			case 40: // down
-			case 83: // s
-				moveBackward = true;
-				break;
+      case 37: // left
+      case 65: // a
+        moveLeft = false;
+        break;
 
-			case 39: // right
-			case 68: // d
-				moveRight = true;
-				break;
-			case 32:
-			//	called = true;
-		//		rising = true;
-				break;
+      case 40: // down
+      case 83: // s
+        moveBackward = false;
+        break;
 
-		}
+      case 39: // right
+      case 68: // d
+        moveRight = false;
+        break;
+    }
+  };
 
-	};
-	var time = 0;
-	var delta = 0;
+  document.addEventListener('keydown', onKeyDown, false);
+  document.addEventListener('keyup', onKeyUp, false);
+  window.addEventListener('resize', onWindowResize, false);
 
+  {
+    const planeSize = 40;
 
-	var onKeyUp = function ( event ) {
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load(
+      'https://threejsfundamentals.org/threejs/resources/images/checker.png'
+    );
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.magFilter = THREE.NearestFilter;
+    const repeats = planeSize / 2;
+    texture.repeat.set(repeats, repeats);
 
-		switch( event.keyCode ) {
+    const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
+    const planeMat = new THREE.MeshPhongMaterial({
+      map: texture,
+      side: THREE.DoubleSide,
+    });
+    const mesh = new THREE.Mesh(planeGeo, planeMat);
+    mesh.rotation.x = Math.PI * -0.5;
+    scene.add(mesh);
+  }
+  {
+    const cubeSize = 4;
+    const cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+    const cubeMat = new THREE.MeshPhongMaterial({ color: '#8AC' });
+    const mesh = new THREE.Mesh(cubeGeo, cubeMat);
+    mesh.position.set(cubeSize + 1, cubeSize / 2, 0);
+    scene.add(mesh);
+  }
+  {
+    const sphereRadius = 3;
+    const sphereWidthDivisions = 32;
+    const sphereHeightDivisions = 16;
+    const sphereGeo = new THREE.SphereGeometry(
+      sphereRadius,
+      sphereWidthDivisions,
+      sphereHeightDivisions
+    );
+    const sphereMat = new THREE.MeshPhongMaterial({ color: '#CA8' });
+    const mesh = new THREE.Mesh(sphereGeo, sphereMat);
+    mesh.position.set(-sphereRadius - 1, sphereRadius + 2, 0);
+    scene.add(mesh);
+  }
 
-			case 38: // up
-			case 87: // w
-				moveForward = false;
-				break;
+  class ColorGUIHelper {
+    constructor(object, prop) {
+      this.object = object;
+      this.prop = prop;
+    }
+    get value() {
+      return `#${this.object[this.prop].getHexString()}`;
+    }
+    set value(hexString) {
+      this.object[this.prop].set(hexString);
+    }
+  }
 
-			case 37: // left
-			case 65: // a
-				moveLeft = false;
-				break;
+  function makeXYZGUI(gui, vector3, name, onChangeFn) {
+    const folder = gui.addFolder(name);
+    folder.add(vector3, 'x', -10, 10).onChange(onChangeFn);
+    folder.add(vector3, 'y', 0, 10).onChange(onChangeFn);
+    folder.add(vector3, 'z', -10, 10).onChange(onChangeFn);
+    folder.open();
+  }
 
-			case 40: // down
-			case 83: // s
-				moveBackward = false;
-				break;
+  {
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.PointLight(color, intensity);
+    light.position.set(0, 10, 0);
+    scene.add(light);
 
-			case 39: // right
-			case 68: // d
-				moveRight = false;
-				break;
+    const helper = new THREE.PointLightHelper(light);
+    scene.add(helper);
 
-			
+    function updateLight() {
+      helper.update();
+    }
 
-		}
-	};
+    const gui = new GUI();
+    gui.addColor(new ColorGUIHelper(light, 'color'), 'value').name('color');
+    gui.add(light, 'intensity', 0, 2, 0.01);
+    gui.add(light, 'distance', 0, 40).onChange(updateLight);
 
-	document.addEventListener( 'keydown', onKeyDown, false );
-	document.addEventListener( 'keyup', onKeyUp, false );
-	window.addEventListener( 'resize', onWindowResize, false );
-	
-	build();
+    makeXYZGUI(gui, light.position, 'position');
+  }
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+
+  build();
 }
 
 function onWindowResize() {
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize( window.innerWidth, window.innerHeight );
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function getScene() {
-	return scene;
+  return scene;
 }
 
 function getControls() {
-	return controls;
+  return controls;
 }
 var step = 1;
 
 function animate() {
+  requestAnimationFrame(animate);
+  if (controlsEnabled == true) {
+    var time = performance.now();
+    var delta = (time - prevTime) / 1000;
 
-	requestAnimationFrame( animate );
-	if ( controlsEnabled == true ) {
+    velocity.x -= velocity.x * 10.0 * delta;
+    velocity.z -= velocity.z * 10.0 * delta;
 
-	 var time = performance.now();
-		var delta =  ( time - prevTime ) / 1000;
+    direction.z = Number(moveForward) - Number(moveBackward);
+    direction.x = Number(moveLeft) - Number(moveRight);
+    //direction.normalize();
 
-		velocity.x -= velocity.x * 10.0 * delta;
-		velocity.z -= velocity.z * 10.0 * delta;
+    if (moveForward || moveBackward) velocity.z -= direction.z * 400.0 * delta;
+    if (moveLeft || moveRight) velocity.x -= direction.x * 400.0 * delta;
+    if (onGround == false) {
+      velocity.x /= 1.1;
+      velocity.z /= 1.1;
+    }
 
-		direction.z = Number( moveForward ) - Number( moveBackward );
-		direction.x = Number( moveLeft ) - Number( moveRight );
-		//direction.normalize();
+    controls.getObject().translateX(velocity.x * delta);
+    controls.getObject().translateZ(velocity.z * delta);
 
-		if ( moveForward || moveBackward ) velocity.z -= direction.z * 400.0 * delta;
-		if ( moveLeft || moveRight ) velocity.x -= direction.x * 400.0 * delta;
-		if (onGround == false) {
-			velocity.x /= 1.1;
-			velocity.z /= 1.1;
-		}
+    prevTime = time;
+  }
 
-		controls.getObject().translateX( velocity.x * delta );
-		controls.getObject().translateZ( velocity.z * delta );
-		
-		prevTime = time; 
+  if (called == true) {
+    jumpRise();
+  } else {
+    step = 1;
+    controls.getObject().position.y = 2;
+  }
 
-	}
-	
-	if (called == true) {
-		
-		jumpRise();
-	}
-	else {
-		step = 1;
-		controls.getObject().position.y = 2;
-	}
+  renderer.render(scene, camera);
+}
 
-	
-	
-	renderer.render( scene, camera );
-	
-	}
-	
-	function jumpRise() {
-		
-		var delta = clock.getDelta();
-		if(controls.getObject().position.y < 10 && rising == true) {
-			controls.getObject().position.y += 1/step;
-			step += 0.3;
-		}
-		else if(controls.getObject().position.y > 9 && falling == false) {
-			rising = false;
-			falling = true;
-		}
-		
-		else if (controls.getObject().position.y < 1 && falling == true) {
-			called = false;
-			falling = false;
-			rising = false;
-		}
-		else if (falling == true) {
-			controls.getObject().position.y -= 1/step;
-			step -= 0.1;
-		}
-		console.log(controls.getObject().position.y);			
-	}
-
+function jumpRise() {
+  var delta = clock.getDelta();
+  if (controls.getObject().position.y < 10 && rising == true) {
+    controls.getObject().position.y += 1 / step;
+    step += 0.3;
+  } else if (controls.getObject().position.y > 9 && falling == false) {
+    rising = false;
+    falling = true;
+  } else if (controls.getObject().position.y < 1 && falling == true) {
+    called = false;
+    falling = false;
+    rising = false;
+  } else if (falling == true) {
+    controls.getObject().position.y -= 1 / step;
+    step -= 0.1;
+  }
+  console.log(controls.getObject().position.y);
+}
 
 init();
 animate();
 
-export {getScene, getControls};
+export { getScene, getControls };
--- js/main.js
+++ js/main.js
@@ -1,35 +1,50 @@
 import * as THREE from './Dependencies/three.module.js';
 import { Grid } from './Grid.js';
 import { CameraController } from './CameraController.js';
+import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/controls/OrbitControls.js';
 
+function main() {
+  const scene = new THREE.Scene();
+  const ratio = window.innerWidth / window.innerHeight;
 
-const scene = new THREE.Scene();
-const ratio = window.innerWidth / window.innerHeight;
+  const camera = new THREE.PerspectiveCamera(45, ratio, 0.1, 1000);
 
-const camera = new THREE.PerspectiveCamera(45, ratio, 0.1, 1000);
+  camera.position.set(13, 25, 30);
+  camera.lookAt(13, 0, 15);
 
-camera.position.set(13, 25, 30);
-camera.lookAt(13, 0, 15);
+  const renderer = new THREE.WebGLRenderer();
+  const sphereRadius = 3;
+  const sphereWidthDivisions = 32;
+  const sphereHeightDivisions = 16;
+  const sphereGeo = new THREE.SphereGeometry(
+    sphereRadius,
+    sphereWidthDivisions,
+    sphereHeightDivisions
+  );
+  const sphereMat = new THREE.MeshPhongMaterial({ color: '#CA8' });
+  const mesh = new THREE.Mesh(sphereGeo, sphereMat);
+  mesh.position.set(-sphereRadius - 1, sphereRadius + 2, 0);
+  scene.add(mesh);
 
-const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
-document.body.appendChild(renderer.domElement);
+  const cameraController = new CameraController(camera, 100);
+  const grid = new Grid(7, 4, camera);
+  scene.add(grid.group);
 
-const cameraController = new CameraController(camera, 100);
+  function animate() {
+    requestAnimationFrame(animate);
 
-const grid = new Grid(7, 4, camera);
-scene.add(grid.group);
+    cameraController.update();
 
-function animate() {
-  requestAnimationFrame(animate);
+    grid.update();
 
-  cameraController.update();
+    renderer.render(scene, camera);
+  }
 
-  grid.update();
-
-  renderer.render(scene, camera);
+  animate();
 }
 
-animate();
+main();
