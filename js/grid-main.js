8a117b70b8b28f673d955a4cd4564b0adc424816
Makar <jsaad41@gmail.com>
2021-05-16 21:28:37 +1000

    update

--- js/grid-main.js
+++ js/grid-main.js
@@ -3,10 +3,15 @@ import { Grid } from "./Grid.js";
 import { CameraController } from "./CameraController.js";
 import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 
+var clock = new THREE.Clock();
+clock.start();
 setup();
+
 
 async function setup () {
   const scene = new THREE.Scene();
+  
+  
 
   const ratio = window.innerWidth / window.innerHeight;
 
@@ -26,12 +31,19 @@ async function setup () {
     });
   }
 
-  const modelUrls = [
+  const modelUrlsForest = [
     './Assets/AurynSky/Forest Pack/Models/Forestground01blender.glb',
     './Assets/AurynSky/Forest Pack/Models/ForestGrassBlender.gltf',
     './Assets/AurynSky/Forest Pack/Models/ForestPineTreeBlender.gltf',
     './Assets/AurynSky/Forest Pack/Models/ForestTreeSmallBlender.glb',
     './Assets/AurynSky/Forest Pack/Models/ForestCrateBlender.glb',
+
+   // './Assets/AurynSky/Dungeon Pack/Models/DungeonBlockBlender.glb',
+    //'./Assets/AurynSky/Dungeon Pack/Models/DungeonBannerBlender.glb'
+  ];
+
+  const modelUrlsSnow = [
+
     './Assets/AurynSky/WinterArena/Models/SnowRockGroundBlender.glb',
     './Assets/AurynSky/WinterArena/Models/SnowPineTreeBlender.glb',
     './Assets/AurynSky/WinterArena/Models/SnowAppleTreeBlender.glb',
@@ -41,8 +53,7 @@ async function setup () {
     './Assets/AurynSky/WinterArena/Models/IcePineTreeBlender.glb',
     './Assets/AurynSky/WinterArena/Models/IceSmallTreeBlender.glb',
     './Assets/AurynSky/WinterArena/Models/IceGrassBlender.glb',
-    './Assets/AurynSky/Dungeon Pack/Models/DungeonBlockBlender.glb',
-    './Assets/AurynSky/Dungeon Pack/Models/DungeonBannerBlender.glb'
+
   ];
 
   const asyncLoadModel = async url => {
@@ -62,7 +73,32 @@ async function setup () {
   }
 
   const getModels = async () => {
-    return Promise.all(modelUrls.map(url => asyncLoadModel(url)));
+    console.log("test");
+    if (clock.getElapsedTime() > 10) {
+
+      clock.stop();
+      clock.start();
+      return Promise.all(modelUrlsForest.map(url => asyncLoadModel(url)));
+    
+    }
+    else {
+      return Promise.all(modelUrlsSnow.map(url => asyncLoadModel(url)));
+    }
+  }
+
+  const updateModels = async (grid) => {
+    console.log("test");
+    if (clock.getElapsedTime() > 10) {
+
+      clock.stop();
+      clock.start();
+      
+      grid.models =  Promise.all(modelUrlsForest.map(url => asyncLoadModel(url)));
+    
+    }
+    else {
+      grid.models = Promise.all(modelUrlsSnow.map(url => asyncLoadModel(url)));
+    }
   }
 
   const models = await getModels();
@@ -73,13 +109,13 @@ async function setup () {
   scene.add(light); 
 
   //3rd person
-  camera.position.set(13, 25, 30);
-  camera.lookAt(13, 0, 15);
+  //camera.position.set(13, 25, 30);
+  //camera.lookAt(13, 0, 15);
 
 
   //first person
-  //camera.position.set(13, 6, 20);
-  //camera.lookAt(13, 0, -10);
+  camera.position.set(13, 6, 20);
+  camera.lookAt(13, 0, -10);
 
   const renderer = new THREE.WebGLRenderer();
 
@@ -94,6 +130,10 @@ async function setup () {
 
   function animate () {
       requestAnimationFrame(animate);
+     //console.log(clock.getElapsedTime());
+      if (clock.getElapsedTime() > 10) {
+       updateModels(grid);
+      }
 
       cameraController.update();
 
